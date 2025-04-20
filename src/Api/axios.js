fcac3c693c19880fbc7caf0beb69dd11a1970c40
hsan-isetsf <hsan.isetsf@gmail.com>
2025-04-20 10:38:07 +0200

    KAN-1 commit-amelioration

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-//baseURL:" https://ecombackend-rouge.vercel.app/api/"
-baseURL:" http://localhost:3002/api/"
+//baseURL:"https://ecombackend-rouge.vercel.app/api/"
+//baseURL:"http://localhost:3002/api/"
+baseURL:"https://2024-juillet-hsan-backend.vercel.app/api/"
 })
